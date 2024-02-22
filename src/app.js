1f108f09324398b153be5b058b6f2a5461108141
YusufLisawi <yusufisawi@gmail.com>
2024-02-23 00:11:35 +0100

    Updates

--- src/app.js
+++ src/app.js
@@ -1,22 +1,10 @@
 import Router from "./router/router.js";
 
 const routes = [
-    {
-        path: "/",
-        component: "../components/home.js",
-    },
-    {
-        path: "/posts",
-        component: "../components/posts.js",
-    },
-    {
-        path: "/posts/:id",
-        component: "../components/post.js",
-    },
-    {
-        path: "/test",
-        component: "../components/example.js",
-    },
+    { path: "/", component: "../components/home.js" },
+    { path: "/posts", component: "../components/posts.js" },
+    { path: "/posts/:id", component: "../components/post.js" },
+    { path: "/test", component: "../components/example.js" }
 ];
 
 class App {
